ded7d8ad4544878e3b45ef4111d66759a2aeb391
zafarr1 <z.rashid1997@gmail.com>
2025-09-06 13:06:25 -0400

    cleaning repo

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "aws-eks-terraform-state-1"       # 👈 replace with your bucket
-    key            = "eks/terraform.tfstate"  # path inside bucket
-    region         = "us-east-1"              # match your region
+    bucket         = "aws-eks-terraform-state-1"       
+    key            = "eks/terraform.tfstate"  
+    region         = "us-east-1"              
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
